53c0ead57d9606770671d9b4b208e2aea128806f
dongna1406410101 <819060452@qq.com>
2017-11-19 16:23:12 +0800

    taohuajian

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -2,6 +2,7 @@
     var ss = document.getElementById("ban");
     ss.style.display = "none";
 }
+
 function ben1() {
     var ss = document.getElementById("hyr");
     var hh = document.getElementById("hh88");
@@ -12,6 +13,7 @@ function ben1() {
     ll.style.color = "black";
     ss.style.display = "none";
 }
+
 function ben2() {
     var ss = document.getElementById("hyr");
     var hh = document.getElementById("hh88");
@@ -22,6 +24,7 @@ function ben2() {
     ll.style.color = "white";
     ss.style.display = "block";
 }
+
 function ben11() {
     var hh = document.getElementById("Button1");
     var ll = document.getElementById("Button2");
@@ -32,6 +35,7 @@ function ben11() {
     hh.style.color = "white";
     ll.style.color = "black";
 }
+
 function ben22() {
     var hh = document.getElementById("Button1");
     var ll = document.getElementById("Button2");
@@ -42,20 +46,24 @@ function ben22() {
     hh.style.color = " black";
     ll.style.color = "white";
 }
+
 function Gan() {
     document.getElementById("tex").value =parseInt(document.getElementById("tex").value)+1;
 
 }
+
 function Gen() {
     if (document.getElementById("tex").value > 0) {
         document.getElementById("tex").value = parseInt(document.getElementById("tex").value) - 1;
     }
 }
+
 function Gea() {
     if (document.getElementById("tat").value > 0) {
         document.getElementById("tat").value = parseInt(document.getElementById("tat").value) - 1;
     }
 }
+
 function Geq() {
     document.getElementById("tat").value = parseInt(document.getElementById("tat").value) + 1;
 }
